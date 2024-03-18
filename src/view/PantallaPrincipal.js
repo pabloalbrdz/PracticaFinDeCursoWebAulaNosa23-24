52485e13e361e05bf69ab213f3f371323caaab61
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2024-03-18 13:27:43 +0100

    #3 QA cambios aplicados

---
+++ src/view/PantallaPrincipal.js
@@ -0,0 +1,30 @@
+import BarraLateral from '../component/BarraLateral.js';
+import PanelOfertas from '../component/PanelOfertas.js';
+import PanelCursos from '../component/PanelCursos.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//librerias necesarias:
+// npm i bootstrap@5.3.3
+// npm install @trendmicro/react-sidenav
+
+function PantallaPrincipal() {
+
+  return (
+    
+    <div className="d-flex vh-100">
+        {/* 16% desplegado  */}
+            {/* 5% sin desplegar  */}
+      <div className="sidebar" style={{ width: '16%' }}>
+        {/* Aquí va el contenido del sidebar */}
+        <BarraLateral />
+      </div>
+      
+      {/* Contenedor de ofertas y cursos */}
+      <div className="d-flex flex-column flex-grow-1">
+      <PanelOfertas/>
+      <PanelCursos/>
+      </div>
+    </div>
+  );
+}
+
+export default PantallaPrincipal;
