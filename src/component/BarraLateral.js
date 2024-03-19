b55ddb050846ff1d142a7720cfbaf93d09942f63
MarcoAntonioCC0 <122261536+MarcoAntonioCC0@users.noreply.github.com>
2024-03-19 09:24:36 +0100

    #3

--- src/component/BarraLateral.js
+++ src/component/BarraLateral.js
@@ -1,92 +1,3 @@
-// import React, { Component } from 'react';
-// import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
-// import "@trendmicro/react-sidenav/dist/react-sidenav.css";
-// class BarraLateral extends Component {
-//   state = {
-//     expanded: false, //por defecto la barra lateral está sin desplegar
-//     showProfileInfo: false // nuevo estado para controlar la visibilidad de la información del perfil
-//   };
-//   toggleExpanded = () => {
-//     this.setState(prevState => ({ expanded: !prevState.expanded }));
-//     // Se agrega un retraso de 300 ms antes de mostrar la información del perfil
-//     setTimeout(() => {
-//       this.setState({ showProfileInfo: this.state.expanded });
-//     }, 100);
-//   };
-//   renderProfileInfo = () => {
-//     // Función para renderizar la información del perfil
-//     return (
-//       <div>
-//         <div className="text-center">
-//           <img src="https://via.placeholder.com/150" alt="User" className="rounded-circle mb-3" />
-//           <h4>Nombre Apellido</h4>
-//         </div>
-//         <hr />
-//         <p>Email: correo@ejemplo.com</p>
-//         <p>Nombre de usuario: usuario123</p>
-//         <p>Otro dato: dato1</p>
-//         <p>Otro dato: dato2</p>
-//         <div className="">
-//           <button className="btn btn-primary btn-block">Editar</button> 
-//           <hr/>       
-//           <button className="btn btn-danger btn-block mb-2">Cerrar sesión</button>
-//         </div>
-//       </div>
-//     );
-//   };
-//   render() {
-//     const { expanded, showProfileInfo } = this.state;
-//     // Renderiza la lista de elementos del SideNav solo si está expandido
-//     const profileInfo = showProfileInfo ? this.renderProfileInfo() : null;
-//     return (
-//       <SideNav expanded={expanded} onToggle={this.toggleExpanded} className="custom-background mt-navbar">
-//         <Toggle />
-//         <Nav>
-//           <NavItem eventKey="perfil">
-//             <NavIcon>
-//               <i className="fa fa-fw fa-user" style={{ fontSize: "1.75em" }} />
-//             </NavIcon>
-//             <NavText>Perfil</NavText>
-//             {profileInfo}
-//           </NavItem>
-//         </Nav>
-//       </SideNav>
-//     );
-//   }
-// }
-// export default BarraLateral;
-
-//--------------------Método collapse----------
-
-
-
-// import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
-// export const BarraLateral = () => {
-//   return (
-//     <div>
-//         <p>
-//   <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
-//     Toggle width collapse
-//   </button>
-// </p>
-// <div style={{ minHeight: '120px' }}>
-//   <div className="collapse collapse-horizontal" id="collapseWidthExample">
-//     <div className="card card-body" style={{ width: '300px' }}>
-//       This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.
-//     </div>
-//   </div>
-// </div>
-//     </div>
-//   )
-// }
-
-
-
-// export default BarraLateral;
-
-//=============================================
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
@@ -94,7 +5,7 @@ import "./BarraLateral.css"
 export const BarraLateral = () => {
   const renderProfileInfo = () => {
     return (
-      <div className='desplegable'>
+      <div>
         <div className="text-center">
           <img src="https://via.placeholder.com/150" alt="User" className="rounded-circle mb-3" />
           <h4>Nombre Apellido</h4>
