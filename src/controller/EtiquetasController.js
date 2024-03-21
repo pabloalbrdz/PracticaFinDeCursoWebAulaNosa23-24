9d45141804e2ed38f2824ec51eb115daf67ea456
alejandrus9 <alepenelas3@gmial.com>
2024-03-21 13:15:42 +0100

    si

--- src/controller/EtiquetasController.js
+++ src/controller/EtiquetasController.js
@@ -1,1 +1,17 @@
-// No definido aun
+import { EtiquetasModel } from "../model/FormacionesModel";
+
+export class EtiquetasController{
+
+    // Metodo que devuelve todas las formaciones
+    static async verTodasEtiquetas(){
+        let response = await EtiquetasController.verTodasEtiquetas();
+        //en caso de que no haya errores hace lo del if
+        if (response.errores != undefined && response.errores.length == 0){
+            return response.etiquetas;
+        }else{
+            //si hay error no devuelve nada
+            return [];
+        }
+    }
+
+}
